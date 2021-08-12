f59e10e0337f3697e6de1b06cc2679896aa1016e
Ankita149 <nitin15067@iiitd.ac.in>
2021-08-12 23:32:11 +0530

    added pdfExport folder and style.css to src

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { AgGridReact, AgGridColumn } from "ag-grid-react";
 import "ag-grid-enterprise";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import PDFExportPanel from "./pdfExport/PDFExportPanel.js";
+import "./style.css";
 
 const App = () => {
     const [paginationPageSize, setPaginationPageSize] = useState(500);
@@ -72,133 +74,101 @@ const App = () => {
     };
 
     return (
-        <div className="table-container">
-            <div className="table-wrapper">
-                <div
-                    id="myGrid"
-                    style={{
-                        height: "100%",
-                        width: "100%",
-                    }}
-                    className="ag-theme-alpine"
-                >
-                    <AgGridReact
-                        defaultColDef={{
-                            resizable: true,
-                            filter: true,
-                        }}
-                        debug={true}
-                        rowSelection={"multiple"}
-                        paginationPageSize={paginationPageSize}
-                        pagination={true}
-                        suppressPaginationPanel={true}
-                        suppressScrollOnNewData={true}
-                        onGridReady={onGridReady}
-                        rowData={rowData}
-                    >
-                        <AgGridColumn
-                            headerName="#"
-                            width={50}
-                            cellRenderer={cellRenderer}
-                        />
-                        <AgGridColumn
-                            headerName="Athlete"
-                            field="athlete"
-                            width={150}
-                        />
-                        <AgGridColumn headerName="Age" field="age" width={90} />
-                        <AgGridColumn
-                            headerName="Country"
-                            field="country"
-                            width={120}
-                        />
-                        <AgGridColumn
-                            headerName="Year"
-                            field="year"
-                            width={90}
-                        />
-                        <AgGridColumn
-                            headerName="Date"
-                            field="date"
-                            width={110}
-                        />
-                        <AgGridColumn
-                            headerName="Sport"
-                            field="sport"
-                            width={110}
-                        />
-                        <AgGridColumn
-                            headerName="Gold"
-                            field="gold"
-                            width={100}
-                        />
-                        <AgGridColumn
-                            headerName="Silver"
-                            field="silver"
-                            width={100}
-                        />
-                        <AgGridColumn
-                            headerName="Bronze"
-                            field="bronze"
-                            width={100}
-                        />
-                        <AgGridColumn
-                            headerName="Total"
-                            field="total"
-                            width={100}
-                        />
-                    </AgGridReact>
-                </div>
-            </div>
-            <div className="pagination-wrapper">
-                <input
-                    type="text"
-                    className="pagination-input"
-                    placeholder="Type here..."
-                />
-                <span className="pagination-info mr-3">
-                    <b>{gridApi ? (currentPageRowCount ? `1` : `0`) : ""}</b> to{" "}
-                    <b>{gridApi ? currentPageRowCount : ""}</b> of{" "}
-                    <b>{gridApi ? rowCount : ""}</b>
-                </span>
-                <span
-                    className={cns(
-                        "pagination-utils",
-                        currentPage === 1 ? "disabled" : ""
-                    )}
-                    onClick={() => onBtFirst()}
-                >{`<<`}</span>
-
-                <span
-                    className={cns(
-                        "pagination-utils",
-                        currentPage === 1 ? "disabled" : ""
-                    )}
-                    onClick={() => onBtPrevious()}
-                >{`<`}</span>
-                <span className="pagination-info">
-                    Page <b>{gridApi ? currentPage : ""}</b> of{" "}
-                    <b>{gridApi ? totalPages : ""}</b>
-                </span>
-                <span
-                    className={cns(
-                        "pagination-utils",
-                        currentPage === totalPages ? "disabled" : ""
-                    )}
-                    onClick={() => onBtNext()}
-                >{`>`}</span>
-                <span
-                    className={cns(
-                        "pagination-utils",
-                        currentPage === totalPages ? "disabled" : ""
-                    )}
-                    onClick={() => onBtLast()}
-                    id="btLast"
-                >
-                    {`>>`}
-                </span>
-            </div>
+      <div className="table-container">
+        <div className="form-wrap">
+          <PDFExportPanel gridApi={gridApi} columnApi={gridColumnApi} />
         </div>
+
+        <div className="table-wrapper">
+          <div
+            id="myGrid"
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+            className="ag-theme-alpine"
+          >
+            <AgGridReact
+              defaultColDef={{
+                resizable: true,
+                filter: true,
+              }}
+              debug={true}
+              rowSelection={"multiple"}
+              paginationPageSize={paginationPageSize}
+              pagination={true}
+              suppressPaginationPanel={true}
+              suppressScrollOnNewData={true}
+              onGridReady={onGridReady}
+              rowData={rowData}
+            >
+              <AgGridColumn
+                headerName="#"
+                width={50}
+                cellRenderer={cellRenderer}
+              />
+              <AgGridColumn headerName="Athlete" field="athlete" width={150} />
+              <AgGridColumn headerName="Age" field="age" width={90} />
+              <AgGridColumn headerName="Country" field="country" width={120} />
+              <AgGridColumn headerName="Year" field="year" width={90} />
+              <AgGridColumn headerName="Date" field="date" width={110} />
+              <AgGridColumn headerName="Sport" field="sport" width={110} />
+              <AgGridColumn headerName="Gold" field="gold" width={100} />
+              <AgGridColumn headerName="Silver" field="silver" width={100} />
+              <AgGridColumn headerName="Bronze" field="bronze" width={100} />
+              <AgGridColumn headerName="Total" field="total" width={100} />
+            </AgGridReact>
+          </div>
+        </div>
+        <div className="pagination-wrapper">
+          <input
+            type="text"
+            className="pagination-input"
+            placeholder="Type here..."
+          />
+          <span className="pagination-info mr-3">
+            <b>{gridApi ? (currentPageRowCount ? `1` : `0`) : ""}</b> to{" "}
+            <b>{gridApi ? currentPageRowCount : ""}</b> of{" "}
+            <b>{gridApi ? rowCount : ""}</b>
+          </span>
+          <span
+            className={cns(
+              "pagination-utils",
+              currentPage === 1 ? "disabled" : ""
+            )}
+            onClick={() => onBtFirst()}
+          >{`<<`}</span>
+
+          <span
+            className={cns(
+              "pagination-utils",
+              currentPage === 1 ? "disabled" : ""
+            )}
+            onClick={() => onBtPrevious()}
+          >{`<`}</span>
+          <span className="pagination-info">
+            Page <b>{gridApi ? currentPage : ""}</b> of{" "}
+            <b>{gridApi ? totalPages : ""}</b>
+          </span>
+          <span
+            className={cns(
+              "pagination-utils",
+              currentPage === totalPages ? "disabled" : ""
+            )}
+            onClick={() => onBtNext()}
+          >{`>`}</span>
+          <span
+            className={cns(
+              "pagination-utils",
+              currentPage === totalPages ? "disabled" : ""
+            )}
+            onClick={() => onBtLast()}
+            id="btLast"
+          >
+            {`>>`}
+          </span>
+        </div>
+      </div>
     );
 };
 
